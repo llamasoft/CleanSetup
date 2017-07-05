a902cc4041188b640bd0a21fa7adbb153095e30a
llamasoft <llamasoft@rm-rf.email>
2017-07-05 11:20:47 -0400

    Tweaking Sublime settings

--- Cygwin/setup.sh
+++ Cygwin/setup.sh
@@ -8,14 +8,14 @@ require() {
     fi
 }
 
-require "curl"
-require "sed"
-require "grep"
+require "curl" # For this script
 
+require "wget" # For apt-cyg
 echo ""
 echo "Installing apt-cyg..."
 aptcyg_url='https://raw.githubusercontent.com/transcode-open/apt-cyg/master/apt-cyg'
 time curl -O "${aptcyg_url}" && install "apt-cyg" "/bin"
+
 
 require "apt-cyg"
 echo ""
@@ -24,6 +24,7 @@ time cat "CygwinPackages.txt" | sed 's/\s*#.*$//' | grep -v '^\s*$' | while read
     echo "Installing ${pkg} ..."
     apt-cyg install "${pkg}"
 done
+
 
 echo ""
 echo "Applying Cygwin tweaks..."
@@ -36,20 +37,35 @@ if [[ -z "${CYGWIN_NOWINPATH}" ]] && command -v setx >/dev/null 2>&1; then
     setx CYGWIN_NOWINPATH 1
 fi
 
+# Sometimes gcc + ld can't find GMP's library files
+# Most libraries have a .a and .dll.a file, but GMP only had the latter
+for lib in "/usr/lib/libgmp" "/usr/lib/libgmpxx"; do
+    if [[ -f "${lib}.dll.a"  && ! -e "${lib}.a" ]]; then
+        ln -s "${lib}.dll.a" "${lib}.a"
+    fi
+done
 
+
+require "perl"
 echo ""
 echo "Installing GNU parallel..."
 parallel_url='https://ftp.gnu.org/gnu/parallel/parallel-latest.tar.bz2'
-time curl -O "${parallel_url}" && tar xjf "parallel-latest.tar.bz2" && cd "parallel-"* && ./configure && make && make install
+time curl -O "${parallel_url}" && tar xjf "parallel-latest.tar.bz2" && cd "parallel-"*"/" && ./configure && make && make install
+echo "will cite" | parallel --citation
+
 
 echo ""
 echo "Installing cpanminus..."
-time cpan App::cpanminus
+cpanminus_url='https://cpanmin.us'
+time curl -L "${cpanminus_url}" | perl - "App::cpanminus"
 echo ""
 
+
+require "python"
 echo "Installing pip..."
 pip_url="https://bootstrap.pypa.io/get-pip.py"
 time curl -O "${pip_url}" && chmod +x "get-pip.py" && python "get-pip.py"
+
 
 echo ""
 echo "Installation complete"
---
+++ Shortcuts.au3
@@ -0,0 +1,75 @@
+﻿#include <Misc.au3>
+#NoTrayIcon
+
+_Singleton("AU3_Shortcuts")
+
+; ! = Alt
+; + = Shift
+; ^ = Control
+; # = Windows
+HotKeySet("^{PAUSE}",   "Quit")
+HotKeySet("^{F6}",      "MoveWindow")
+
+HotKeySet("#{NUMPAD7}", "VolDown")
+HotKeySet("#{NUMPAD8}", "PlayPause")
+HotKeySet("#{NUMPAD9}", "VolUp")
+HotKeySet("#{NUMPAD4}", "SongPrev")
+HotKeySet("#{NUMPAD6}", "SongNext")
+
+HotKeySet("#{NUMPAD1}", "Calculator")
+HotKeySet("#{NUMPAD2}", "CommandPrompt")
+HotKeySet("#{NUMPAD3}", "Cygwin")
+
+
+While (1)
+   Sleep(500)
+WEnd
+
+
+Func Quit()
+    ToolTip("Exiting...", 0, 0)
+    Sleep(3000)
+	Exit
+EndFunc
+
+
+; Misc Functions
+Func MoveWindow()
+	WinMove("[ACTIVE]", "", 0, 0)
+EndFunc
+
+
+; Media Keys
+Func VolDown()
+    Send("{VOLUME_DOWN}")
+EndFunc
+
+Func VolUp()
+    Send("{VOLUME_UP}")
+EndFunc
+
+Func PlayPause()
+    Send("{MEDIA_PLAY_PAUSE}")
+EndFunc
+
+Func SongNext()
+    Send("{MEDIA_NEXT}")
+EndFunc
+
+Func SongPrev()
+    Send("{MEDIA_PREV}")
+EndFunc
+
+
+; Program Shortcuts
+Func Calculator()
+    Run("calc.exe")
+EndFunc
+
+Func CommandPrompt()
+    Run("cmd.exe")
+EndFunc
+
+Func Cygwin()
+    Run("C:\cygwin64\bin\mintty.exe -i /Cygwin-Terminal.ico -")
+EndFunc
